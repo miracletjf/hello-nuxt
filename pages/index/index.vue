0f5805e4b970c052f0ca2e321438a10ff0e88608
miracletjf <1398988546@qq.com>
2019-10-13 23:13:17 +0800

    使用 nuxt 搭建 服务端渲染 html 示例

---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{name}}</h1>
+    <!-- <ul v-if="listData && listData.length">
+      <li v-for="(item, index) in listData" :key="index">
+        <a :href="item.url">{{item.name}}</a>
+        <span class="text">{{item.address}}</span>
+      </li>
+    </ul> -->
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+    // const listData = await $axios.$get('https://www.easy-mock.com/mock/5bd01284dfa1f337facb9fe0/example/mock')
+    const result = await $axios.$get('https://www.easy-mock.com/mock/5bd01284dfa1f337facb9fe0/example/query')
+    let name = result.data.name
+    console.log(result.data.name)
+    return { name }
+  },
+  data() {
+    return { }
+  },
+  created() {
+    this.initData()
+  },
+  methods: {
+    initData(){
+      this.getUserInfo()
+    },
+    getUserInfo() {
+      console.log(this.$route.query)
+      // this.name = this.$route.query.name
+    }
+  }
+}
+</script>
